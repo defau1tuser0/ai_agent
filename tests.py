d6ebe5f820167616372760426e9516dec4e73f7a
defau1tuser0 <iaditya8424@gmail.com>
2025-08-30 10:47:05 +0000

    write_file.py

--- tests.py
+++ tests.py
@@ -1,23 +1,15 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' file in 'calculator directory:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py' file in 'pkg' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat'file in 'calculator' directory:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for 'pkg/does_not_exist.py' file in 'calculator' directory:")
-    print(result)
-
 
 if __name__ == "__main__":
     test()
