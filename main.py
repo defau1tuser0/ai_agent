3c163c1cb983370cb273b76ae8638a06131549da
defau1tuser0 <iaditya8424@gmail.com>
2025-08-30 02:50:38 +0000

    added the calc project for ai_agent to work on

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,16 +11,26 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 if len(sys.argv) > 1:
-	prompt = sys.argv[1] 
-	print("---------")
-	print(prompt)
-	print("---------")
-	response = client.models.generate_content(
-    	model="gemini-2.0-flash-001", contents=prompt + " in short"
-	)
+    user_prompt = sys.argv[1]
 
-	print(response.text)
-	print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-	print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+    )
+    
+    print(response.text)
+
+    try: 
+        if sys.argv[2] and sys.argv[2] == "--verbose":
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except Exception as e:
+        pass
+   
 else:
-	print("No prompt given")
+    print("No prompt given")
+    sys.exit(1)
